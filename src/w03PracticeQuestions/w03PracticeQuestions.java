41ec5e5925dd6329ebe1244348cca4e64839baeb
sbailey-27 <162745220+sbailey-27@users.noreply.github.com>
2024-06-18 16:05:07 -0400

    switch

---
+++ src/w03PracticeQuestions/w03PracticeQuestions.java
@@ -0,0 +1,9 @@
+package w03PracticeQuestions;
+
+public class w03PracticeQuestions {
+    public static void main(String [] args){
+        int counter = 0;
+        int num1;
+        int num2;
+    }
+}
--- src/w03SwitchStatements/w03SwitchStatements.java
+++ src/w03SwitchStatements/w03SwitchStatements.java
@@ -1,7 +1,42 @@
 package w03SwitchStatements;
 
+import javax.swing.*;
+
 public class w03SwitchStatements {
     public static void main (String[] args){
+        String usersGrade;
+        String output = "";
+        char grade;
+
+        usersGrade = JOptionPane.showInputDialog("Please enter your grade (a-f)");
+        grade = usersGrade.charAt(0);
+
+        switch (grade){
+            case 'a':
+                output = "Fantastic result";
+                break;
+            case 'b':
+                output = "Great result";
+                break;
+            case 'c':
+                output = "Very good";
+                break;
+            case 'd':
+                output = "Must do better";
+                break;
+            case 'e':
+                output = "Poor";
+                break;
+            case 'f':
+                output = "Fail";
+                break;
+            default:
+                output = "I did not recognise your entry";
+        }
+
+        JOptionPane.showMessageDialog(null, output, "",
+                JOptionPane.INFORMATION_MESSAGE);
+
 
     }
 }
